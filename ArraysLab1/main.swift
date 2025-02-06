dbb79178083303741535a194bcf202abff3ad5c4
Jasmine Fernandez <jafernandez7@cps.edu>
2025-02-06 11:47:23 -0600

    blebdfe

--- ArraysLab1/main.swift
+++ ArraysLab1/main.swift
@@ -112,7 +112,7 @@ print(registrationList)
 print("***PROBLEM 1.1 : BASIC ARRAY ADDITION")
 // Your friend Aster is the first to register for the event. Add her name to registrationList using the append(_:) method. Print the contents of the collection.
 //Hint: See lines 35 & 48
-registrationList.append("sara")
+registrationList.append("Sara")
 print(registrationList)
  
 print("\n")
@@ -123,7 +123,9 @@ print("***PROBLEM 1.2 : MORE ARRAY ADDITIONS")
 //This is how you do this: 1. Create a new array called moreNames and fill it with four new names.
 // 2. Then add that new array to your original array. Yes ADD.
 //How? To add in multiple names in one step use the += operator we will need to have the names in a separate array that contains the names. Then print your registrationList and observe the output in the console.
-
+var moreNames : [String] = ["Mike"," Charles"," Jack"," Fin"]
+registrationList += moreNames
+print(registrationList)
 
  print("\n")
  
@@ -132,9 +134,8 @@ print("***PROBLEM 1.3 : ARRAY INSERTION")
 //Warning: The second element is equivalent to the first index, since we begin counting from 0. Think about it, it's tricky. 0 is the first element and 1 is the second element.
  //Hint: see line 78
 //print the registration list and observe the output making sure it is correct.
-
-
-
+registrationList.insert("Charlie" , at: 1)
+print(registrationList)
  print("\n")
  
 print("***Example for 1.4")
@@ -149,16 +150,16 @@ print("***PROBLEM 1.4 : ARRAY MODIFICATION")
  //See the example above and remember the sixth element is the fifth index.
 // registrationList[1] = "Charlie1"
 // print(registrationList)
-
-
+registrationList[5]="Rebecca"
+print(registrationList)
  print("\n")
  
  
 print("***PROBLEM 1.5 : ARRAY REMOVAL")
 // Call removeLast() on registrationList. Store the result of removeLast() into a new constant deletedItem. If done correctly, this should remove Rebecca from the collection. print the registrationList, then print deletedItem.
 //Observe the output in the console and make sure it is correct.
-
- 
+let deletedItem = registrationList.removeLast()
+ print(registrationList)
 print("\n")
 
 print("***PROBLEM 2.0 : FITNESS TRACKING")
